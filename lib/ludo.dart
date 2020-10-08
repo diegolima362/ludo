6f6e945972935b04ca7b2a9a17e570f9091d216b
diegolima362 <diegolima362@gmail.com>
2020-10-08 19:58:22 -0300

    adicao da lib flame

---
+++ lib/ludo.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class Ludo extends Game with TapDetector {
+  @override
+  void render(Canvas c) {}
+
+  @override
+  void update(double t) {}
+
+  @override
+  void resize(Size size) {}
+
+  @override
+  void onTapDown(TapDownDetails d) {}
+}
