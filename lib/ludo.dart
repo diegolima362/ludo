f3a3994ccc10011311575f548cda828c578bb12e
diegolima362 <diegolima362@gmail.com>
2020-10-08 20:03:44 -0300

    add background

--- lib/ludo.dart
+++ lib/ludo.dart
@@ -2,18 +2,38 @@ import 'dart:ui';
 
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'util/util.dart';
+
 class Ludo extends Game with TapDetector {
+  Size screenSize;
+
+  void initialize() async {}
+
+  Ludo() {
+    initialize();
+  }
+
   @override
-  void render(Canvas c) {}
+  void render(Canvas c) {
+    _drawBackground(c);
+  }
+
+  void _drawBackground(Canvas c) {
+    Rect background = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint backgroundPaint = Paint()..color = AppColors.backgroundColor;
+
+    c.drawRect(background, backgroundPaint);
+  }
 
   @override
   void update(double t) {}
 
   @override
-  void resize(Size size) {}
+  void resize(Size size) {
+    screenSize = size;
+  }
 
   @override
   void onTapDown(TapDownDetails d) {}
---
+++ lib/util/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color get backgroundColor => Colors.deepPurple;
+
+  static const List<Color> colors = [
+    Colors.green,
+    Colors.red,
+    Colors.blue,
+    Colors.yellow,
+  ];
+
+  static Color get home1 => colors[0];
+
+  static Color get home2 => colors[1];
+
+  static Color get home3 => colors[2];
+
+  static Color get home4 => colors[3];
+
+  static Color get activeHome => Colors.purpleAccent;
+
+  static Color get player1 => colors[0];
+
+  static Color get player2 => colors[1];
+
+  static Color get player3 => colors[2];
+
+  static Color get player4 => colors[3];
+
+  static Color get safeSpot => Colors.grey;
+
+  static Color get rollDice => Colors.amber;
+}
